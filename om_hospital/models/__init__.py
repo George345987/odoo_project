c9256526ad34f5f7ff3447302f9b07080dfdea44
GeorgeRaafat <GeorgeRaafat@example.com>
2025-09-15 18:56:04 +0300

    Initial commit

---
+++ om_hospital/models/__init__.py
@@ -0,0 +1,10 @@
+from . import patient
+from . import appointment
+
+echo "# odoo_project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/George345987/odoo_project.git
+git push -u origin main
